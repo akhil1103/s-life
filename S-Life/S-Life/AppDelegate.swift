3e1c3f01bd9c94064a726b2365b7244f57692890
balabalaji <>
2021-08-02 22:02:23 +0530

    Project Creation

---
+++ S-Life/S-Life/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  S-Life
+//
+//  Created by balabalaji gowd yelagana on 02/08/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
